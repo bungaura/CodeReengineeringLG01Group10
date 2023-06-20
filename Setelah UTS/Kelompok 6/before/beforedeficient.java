de7eb2de793ff4e80fa4959c621b9ac393d78a4b
Bunga Aura Prameswari <88443222+bungaura@users.noreply.github.com>
2023-06-20 12:17:15 +0700

    Add files via upload

---
+++ Setelah UTS/Kelompok 6/before/beforedeficient.java
@@ -0,0 +1,13 @@
+package before;
+public class Rectangle {
+    public int width;
+    public int height;
+
+    public int calculateArea() {
+        return width * height;
+    }
+
+    public int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
