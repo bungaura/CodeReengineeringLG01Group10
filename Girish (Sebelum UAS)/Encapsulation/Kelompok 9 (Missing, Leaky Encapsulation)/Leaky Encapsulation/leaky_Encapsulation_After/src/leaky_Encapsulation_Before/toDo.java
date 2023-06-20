e7ea5f0846b9130c768d8eb43e5172649e0514d9
Bunga Aura Prameswari <88443222+bungaura@users.noreply.github.com>
2023-06-20 20:17:53 +0700

    Add files via upload

---
+++ Girish (Sebelum UAS)/Encapsulation/Kelompok 9 (Missing, Leaky Encapsulation)/Leaky Encapsulation/leaky_Encapsulation_After/src/leaky_Encapsulation_Before/toDo.java
@@ -0,0 +1,23 @@
+package leaky_Encapsulation_Before;
+
+
+
+class ToDo {
+   
+    private String name;
+
+    public ToDo(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
