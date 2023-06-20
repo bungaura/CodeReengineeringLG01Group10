01405403250da0df51b8e473f1a64ccd67a6f7b0
Bunga Aura Prameswari <88443222+bungaura@users.noreply.github.com>
2023-06-20 20:24:53 +0700

    Delete Setelah UTS directory

--- Setelah UTS/Kelompok 14/Unnecessary Hierarchy/After/Unnecessary-Hierarchy-main/UnnecessaryAfter/src/ThreeDimensionalShape.java
+++
@@ -1,4 +0,0 @@
-
-public interface ThreeDimensionalShape extends Shape {
-    double calculateVolume();
-}
--- Setelah UTS/Kelompok 4/Incomplete Library/Before/EmailService.java
+++
@@ -1,6 +0,0 @@
-
-public class EmailService {
-	public void sendEmail(String to, String subject, String body) {
-        // This method is not fully implemented
-    }
-}
