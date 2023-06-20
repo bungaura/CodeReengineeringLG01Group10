0124c6ec0791bcc31fdb611adcf97c5ec3013a50
Bunga Aura Prameswari <88443222+bungaura@users.noreply.github.com>
2023-06-20 20:13:30 +0700

    Add files via upload

---
+++ Martin Fowler (Sebelum UTS)/The Bloater/Kelompok 2 (Long Parameter List, Data Clumps)/After/Long Parameter List - After/After/Wallet.java
@@ -0,0 +1,22 @@
+package LongParameterList.After;
+
+public class Wallet {
+	private int balance;
+	
+	public Wallet() {
+		super();
+		balance = 0;
+	}
+
+	public void add(int amount) {
+		balance += amount;
+	}
+	
+	public void reduce(int amount) {
+		balance -= amount;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+}
